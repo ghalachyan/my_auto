0bb4e04d518f39d448fe670371859a50d16e0073
Misha Ghalachyan <mish.ghalachyan@gmail.com>
2024-10-04 17:27:49 +0400

    Create a project that will allow you to drop announcements․

---
+++ app.js
@@ -0,0 +1,16 @@
+import 'dotenv/config.js';
+import morgan from 'morgan';
+import express from 'express';
+
+import indexRouter from './routes/index.js';
+
+const port = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use('/', indexRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
